52f0392d950f56d5f00b6916c2c9aaff61d75bba
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2015-12-03 22:47:03 +0000

    Revert "Revert "Login é merda""
    This reverts commit 63a105c8084b793254b0a675685e62f9ffd56648.

---
+++ places4all/places4all/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace places4all
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ places4all/places4all/Models/Rate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace places4all.Models
+{
+    public class Rate
+    {
+        public int RateId { get; set; }
+        public int Grade { get; set; }
+    }
+    public class RateDBContext : DbContext
+    {
+        public DbSet<Rate> Rates { get; set; }
+    }
+}
