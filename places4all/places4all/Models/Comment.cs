6d880fa833a4803811390e167addb24d04a6b57e
Mafalda Falcão <mafaldafalcaotvf@gmail.com>
2015-12-03 16:59:40 +0000

    Login é merda

---
+++ places4all/places4all/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace places4all.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+    public class CommentDBContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ places4all/places4all/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(places4all.Startup))]
+namespace places4all
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
