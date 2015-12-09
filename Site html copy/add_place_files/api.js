d55e14eebda22f54dd3095d41f5a883afe0a014d
PedroFaria <pedrommlcfaria@gmail.com>
2015-12-09 17:43:42 +0000

    added css and js code to respective folders

--- Site html copy/add_place_files/api.js
+++ Site html copy/add_place_files/api.js
@@ -1,1 +1,16 @@
-/* PLEASE DO NOT COPY AND PASTE THIS CODE. */(function() {if (!window['___grecaptcha_cfg']) { window['___grecaptcha_cfg'] = {}; };if (!window['___grecaptcha_cfg']['render']) { window['___grecaptcha_cfg']['render'] = 'onload'; };window['__google_recaptcha_client'] = true;var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://www.gstatic.com/recaptcha/api2/r20151104115408/recaptcha__pt_pt.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */
+(function() {
+    if (!window['___grecaptcha_cfg']) {
+        window['___grecaptcha_cfg'] = {};
+    };
+    if (!window['___grecaptcha_cfg']['render']) {
+        window['___grecaptcha_cfg']['render'] = 'onload';
+    };
+    window['__google_recaptcha_client'] = true;
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://www.gstatic.com/recaptcha/api2/r20151104115408/recaptcha__pt_pt.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+})();
