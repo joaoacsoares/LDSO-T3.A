0f3e1b27284733a9310b045ecaaf5a39d38111dd
JoaoSoares <joaosoaresacs@gmail.com>
2015-12-29 12:01:00 +0000

    added Howitworks and site area -> Views need fixing!

---
+++ places4all/places4all/Controllers/SiteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace places4all.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Media()
+        {
+            return View();
+        }
+        public ActionResult Partners()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
